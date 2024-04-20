e20a866e2d50ebcaeea1cea652b0b96c2d64dc0b
Emmanuel Amiteye <emmanuelamiteye9@gmail.com>
2024-04-20 18:12:36 +0100

    3d model

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -1,13 +1,17 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import * as THREE from 'three';
+
 import ModelView from './ModelView';
 import { yellowImg } from '../utils';
 
 
+
+
 const Model = () => {
-    const [size, setSize ] = useState('small');
-    const [ model, setModel] = useState({
+    const [size, setSize] = useState('small');
+    const [model, setModel] = useState({
         title: 'Iphone 15 pro in Natural Titanium',
         color: [
             '#8F8AB1',
@@ -17,25 +21,55 @@ const Model = () => {
         img: yellowImg,
     })
 
+    // camera control for the model view
+    const cameraControlSmall = useRef();
+    const cameraControlLarge = useRef();
+
+    // models
+    const small = useRef(new THREE.Group());
+    const large = useRef(new THREE.Group());
+
+    // Rotation Value
+    const [smallRotation, setSmallRotation] = useState(0);
+    const [largeRotation, setLargeRotation] = useState(0);
+
     useGSAP(() => {
-        gsap.to('#heading', { y: 0, opacity: 1})
+        gsap.to('#heading', { y: 0, opacity: 1 })
     }, [])
 
     return (
-    <section className='common-padding'>
-      <div className='screen-max-width'>
-         <h1 id='heading' className='section-heading'> 
-           Take a closer look
-         </h1>
+        <section className='common-padding'>
+            <div className='screen-max-width'>
+                <h1 id='heading' className='section-heading'>
+                    Take a closer look
+                </h1>
 
-         <div className='flex flex-col items-center mt-5'>
-            <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
-                <ModelView/>
+                <div className='flex flex-col items-center mt-5'>
+                    <div className='w-full h-[75vh] md:h-[90vh] overflow-hidden relative'>
+                        <ModelView
+                            index={1}
+                            groupRef={small}
+                            gsapType="view1"
+                            controlRef={cameraControlSmall}
+                            setRotationState={setSmallRotation}
+                            item={model}
+                            size={size}
+                        />
+
+                        <ModelView
+                            index={2}
+                            groupRef={large}
+                            gsapType="view2"
+                            controlRef={cameraControlLarge}
+                            setRotationState={setLargeRotation}
+                            item={model}
+                            size={size}
+                        />
+                    </div>
+                </div>
             </div>
-         </div>
-      </div>
-    </section>
-  )
+        </section>
+    )
 }
 
 export default Model
