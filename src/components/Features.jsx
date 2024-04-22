4bf796a8758fd38822fc9c0be76924a044b592f5
Emmanuel Amiteye <emmanuelamiteye9@gmail.com>
2024-04-22 14:24:24 +0100

    features

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,22 +1,68 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
-import React from 'react'
+import React, { useRef } from 'react'
+import { animateWithGsap } from '../utils/animations'
+import { explore1Img, explore2Img, exploreVideo } from '../utils'
 
 const Features = () => {
     useGSAP(() => {
-        gsap.to('#features_title', {
-            y: -50,
-            opacity: 1,
-            duration: 2
+        animateWithGsap('#features_title', {
+            y: 0,
+            opacity: 1
         })
+
+        animateWithGsap('.g_grow', {
+            scale: 1,
+            opacity: 1,
+            ease: 'power1' },
+            {scrub: 5.5}
+        )
     }, [])
+
+    const videoRef = useRef();
     return (
         <section className='h-full common-padding bg-zinc overflow-hidden relative'>
             <div className='screen-max-width'>
                 <div className='mb-12 w-full'>
                     <h1 id='features_title' className='section-heading'>
-                        Explore the full story. 
+                        Explore the full story.
                     </h1>
+                </div>
+
+                <div className='flex flex-col justify-center items-center overflow-hidden '>
+                    <div className='mt-32 mb-24 pl-24'>
+                        <h2 className='text-5xl lg:text-7xl font-semibold'> Iphone</h2>
+                        <h2 className='text-5xl lg:text-7xl font-semibold'> Forged In Titanium.</h2>
+                    </div>
+
+                    <div className='flex-center flex-col sm:p-10'>
+                        <div className='relative h-[50vh] w-full flex items-center'>
+                            <video
+                                playsInline
+                                id='exploreVideo'
+                                className='w-full h-full object-cover object-center'
+                                preload='none'
+                                muted
+                                autoPlay
+                                ref={videoRef}
+                            >
+                                <source src={exploreVideo} type='video/mp4' />
+                            </video>
+                        </div>
+                        <div className='flex flex-col w-full relative'>
+                            <div className='feature-video-container'>
+                                <div className='overflow-hidden flex-1 h-[50vh]'>
+                                    <img src={explore1Img} alt='titanium' className='feature-video g_grow' />
+                                </div>
+
+                                <div className='overflow-hidden flex-1 h-[50vh]'>
+                                    <img src={explore2Img} alt='titanium' className='feature-video g_grow' />
+                                </div>
+
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
             </div>
         </section>
